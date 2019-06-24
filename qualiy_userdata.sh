17a6db682aef93237e5bcbf0abe27316b098a6e6
Gil <GilGald@users.noreply.github.com>
2019-06-24 17:55:41 +0300

    Update qualiy_userdata.sh

--- qualiy_userdata.sh
+++ qualiy_userdata.sh
@@ -26,10 +26,10 @@ echo 'Staring guacamole-client...'
 
 #if no env variables 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 quali/guacamole-client:1.0.0
+	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 -p 8000:8000 dockergil90/guacamole-client-debugable:1.0
 else 
 	#bucket name ,region,key name
-	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080  -e $1 -e $2 -e $3 quali/guacamole-client:1.0.0
+	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 -p 8000:8000 -e $1 -e $2 -e $3 dockergil90/guacamole-client-debugable:1.0
 fi
 
 echo 'End of QualiY user-data'
